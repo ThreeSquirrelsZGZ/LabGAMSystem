fe2bfc6255974e564c6f7d3f0017d9a9b0bf5d90
GuYue <gyuue@qq.com>
2018-12-05 23:21:06 +0800

    1205celue

---
+++ src/com/xafdy/alg/HolidayStrategy.java
@@ -0,0 +1,15 @@
+package com.xafdy.alg;
+
+public class HolidayStrategy {
+	//int holidays[]={9,11,9,10,9,10,9,8,9,13,8,10};
+	Holidays h;
+	public double arrangeholidays(int year,int month,int state){
+		if(state==1){
+			h=new CriticalHolidays();
+			
+		}else{
+			h=new NormalHolidays();
+		}
+		return h.arrangeholidays(year,month);
+	}
+}
